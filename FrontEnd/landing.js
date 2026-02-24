4b8e1837f45a8320639bcd9216c43520f49ec2d9
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2026-02-23 20:31:07 -0700

    skeleton frontend with javascript

---
+++ FrontEnd/landing.js
@@ -0,0 +1,14 @@
+var pointALon = document.getElementById('pointALON')
+var pointALat = document.getElementById('pointALAT')
+var pointBLon = document.getElementById('pointBLON')
+var pointBLat = document.getElementById('pointBLAT')
+var streetName = document.getElementById('streetName')
+
+
+function clear(){
+    pointALat.value = ''
+    pointALon.value = ''
+    pointBLat.value = ''
+    pointBLon.value = ''
+    streetName.value = ''
+}
